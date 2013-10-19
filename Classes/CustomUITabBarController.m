b0b5c68ac683872bc378e24f180bbb3185b96386
Benjamin Baron <ben@einsteinx2.com>
2013-10-18 17:35:15 -0700

    - Fixed disable rotation setting doesn't work

---
+++ Classes/CustomUITabBarController.m
@@ -0,0 +1,30 @@
+//
+//  CustomUITabBarController.m
+//  iSub
+//
+//  Created by Benjamin Baron on 10/18/13.
+//  Copyright (c) 2013 Ben Baron. All rights reserved.
+//
+
+#import "CustomUITabBarController.h"
+
+@interface CustomUITabBarController ()
+
+@end
+
+@implementation CustomUITabBarController
+
+- (BOOL)shouldAutorotate
+{
+    return [self shouldAutorotateToInterfaceOrientation:[UIDevice currentDevice].orientation];
+}
+
+-(BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)inOrientation
+{
+	if (settingsS.isRotationLockEnabled && inOrientation != UIInterfaceOrientationPortrait)
+		return NO;
+	
+    return YES;
+}
+
+@end
