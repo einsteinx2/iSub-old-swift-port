c845ae25c1eec0212035b43257796dd06aee07b9
Ben Baron <ben@einsteinx2.com>
2016-02-10 23:41:50 -0800

    Cleaned up CancelBlocks

--- CancelBlocks.swift
+++ CancelBlocks.swift
@@ -8,30 +8,60 @@
 
 typealias dispatch_cancelable_block_t = (cancel:Bool) -> (Void)
 
-func dispatch_block_t(delay:Double, block:dispatch_block_t?) -> dispatch_cancelable_block_t? {
-    if (block == nil) {
-        return nil
-    }
-    var originalBlock:dispatch_block_t? = block!
-    var cancelableBlock:dispatch_cancelable_block_t? = nil
-    var delayBlock:dispatch_cancelable_block_t = {(cancel:Bool) -> Void in
-        if ((!cancel) && (originalBlock != nil)) {
+func dispatch_block_t(delay: Double, block: dispatch_block_t) -> dispatch_cancelable_block_t {
+    var originalBlock: dispatch_block_t? = block
+    var cancelableBlock: dispatch_cancelable_block_t? = nil
+    
+    let delayBlock: dispatch_cancelable_block_t = { cancel in
+        if !cancel && originalBlock != nil {
             dispatch_async(dispatch_get_main_queue(), originalBlock!)
         }
         cancelableBlock = nil
         originalBlock = nil
     }
     cancelableBlock = delayBlock
+    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
-        if (cancelableBlock != nil) {
-            cancelableBlock!(cancel: false)
+        if let cancelableBlock = cancelableBlock {
+            cancelableBlock(cancel: false)
         }
     }
-    return cancelableBlock
+    
+    return cancelableBlock!
 }
 
 func dispatch_cancel_block_t(block:dispatch_cancelable_block_t?) {
-    if (block != nil) {
-        block!(cancel: true)
+    if let block = block {
+        block(cancel: true)
     }
 }
+
+
+// Original code
+//func dispatch_block_t(delay:Double, block:dispatch_block_t?) -> dispatch_cancelable_block_t? {
+//    if (block == nil) {
+//        return nil
+//    }
+//    var originalBlock:dispatch_block_t? = block!
+//    var cancelableBlock:dispatch_cancelable_block_t? = nil
+//    var delayBlock:dispatch_cancelable_block_t = {(cancel:Bool) -> Void in
+//        if ((!cancel) && (originalBlock != nil)) {
+//            dispatch_async(dispatch_get_main_queue(), originalBlock!)
+//        }
+//        cancelableBlock = nil
+//        originalBlock = nil
+//    }
+//    cancelableBlock = delayBlock
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
+//        if (cancelableBlock != nil) {
+//            cancelableBlock!(cancel: false)
+//        }
+//    }
+//    return cancelableBlock
+//}
+//
+//func dispatch_cancel_block_t(block:dispatch_cancelable_block_t?) {
+//    if (block != nil) {
+//        block!(cancel: true)
+//    }
+//}
