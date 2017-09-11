fedcf9a82c22481d5e4e8ac3d2f258c0369d55ac
Ben Baron <ben@einsteinx2.com>
2017-09-10 20:03:33 -0500

    Fixed broken 3D nav stack

--- Frameworks/NavigationStack/NavigationStack.swift
+++ Frameworks/NavigationStack/NavigationStack.swift
@@ -166,7 +166,7 @@ extension UIView {
     func takeScreenshot() -> UIImage {
         
         UIGraphicsBeginImageContextWithOptions(self.bounds.size, false, UIScreen.main.scale)
-        drawHierarchy(in: self.bounds, afterScreenUpdates: true)
+        drawHierarchy(in: self.bounds, afterScreenUpdates: false)
         
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
